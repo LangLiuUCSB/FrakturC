36453c0dcd416824eca2cf75d9a3588298662cc3
LangLiuUCSB <157067894+LangLiuUCSB@users.noreply.github.com>
2024-11-09 13:24:04 -0800

    hygiene

--- main.c
+++ main.c
@@ -6,26 +6,10 @@
 
 int main(void)
 {
-    _num *n1 = wrapNnum(1);
-    printx_num(n1);
-    ln;
-    print_size(n1);
-    ln;
-
-    _num *n2 = wrapNnum(2);
-    printx_num(n2);
-    ln;
-    print_size(n2);
-    ln;
-
-    addto_num(n1, n2);
-    printx_num(n1);
-    ln;
-    print_size(n1);
-    ln;
-
-    free_num(n1);
-    free_num(n2);
+    int8_t num = -128;
+    // lower limit is -128
+    // upper limit is 127
+    printf("%hhx", num);
 
     ln;
     return EXIT_SUCCESS;
